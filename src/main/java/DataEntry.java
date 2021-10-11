d0b26a7a5d6ebbb94df3f11b9eab92c2f358093f
Vasyl Danylenko <Saifferela@gmail.com>
2021-10-11 17:30:05 +0300

    Added cyclic input on error.

---
+++ src/main/java/DataEntry.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class DataEntry {
+
+    Scanner console;
+    private byte fibonacciNumber;
+
+    public DataEntry(Scanner console) {
+        this.console = console;
+    }
+
+    //Setter - Getter number
+
+    public void setNumber(){
+
+        while(!console.hasNextByte())
+        {
+            System.out.print("Введите число от 0 до 91 или Ctrl+C для выхода: ");
+            console.next();
+        }
+
+        fibonacciNumber = console.nextByte();
+    }
+
+    public byte getNumber() {
+        return fibonacciNumber;
+    }
+}
